1dcf470c834ae2d2c961bb1d8ac21eabe3019fb0
Brian Lavery <brian.lavery@yahoo.co.uk>
2022-06-14 15:10:12 +0700

    commit

---
+++ app/models/restaurant.rb
@@ -0,0 +1,15 @@
+class Restaurant < ApplicationRecord
+    # Associations: Cuisines
+    has_many :restaurant_cuisines, dependent: :destroy
+    has_many :cuisines, through: :restaurant_cuisines
+    
+    # PgSearch setup
+    include PgSearch::Model
+    pg_search_scope :search_by_location,
+      against: [:location],
+      using: {
+        tsearch: { prefix: true }
+      }
+    
+    
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_06_14_064557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "cuisines", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "restaurant_cuisines", force: :cascade do |t|
+    t.bigint "restaurant_id", null: false
+    t.bigint "cuisine_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cuisine_id"], name: "index_restaurant_cuisines_on_cuisine_id"
+    t.index ["restaurant_id"], name: "index_restaurant_cuisines_on_restaurant_id"
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "restaurant_cuisines", "cuisines"
+  add_foreign_key "restaurant_cuisines", "restaurants"
 end
